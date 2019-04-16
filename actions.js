c55359f7aa00c1d62c5221683a0345fc80bf605e
Mircea <bmircea23@gmail.com>
2019-04-16 08:57:25 +0300

    Updated some aspects. Solved BUGS. Added more currencies

--- actions.js
+++ actions.js
@@ -30,6 +30,12 @@ $("#switchMain").click(function(){
 var valori = {
 	"EUR" : 4.74414,
 	"USD" : 4.22290,
+	"GBP" : 5.51103,
+	"INR" : 0.0605903,
+	"JPY" : 0.0376016,
+	"NOK" : 0.495697,
+	"CNY" : 0.627444,
+	"SEK" : 0.454109,
 	"RON" : 1
 
 };
@@ -53,9 +59,16 @@ $("#convMain").click(function(){
 
 $("#convJumbo").click(function(){
 	$("#inputJos").val($("#inputSus").val());
-	$("#rezultat-conversie").text($("#inputSus").val());
 	$("#dropdownMenuButtonFromMain").text($("#dropdownMenuButtonFromJumbo").text());
 	$("#dropdownMenuButtonToMain").text($("#dropdownMenuButtonToJumbo").text());
+    
+    var from = $("#dropdownMenuButtonFromMain").text();
+	var to = $("#dropdownMenuButtonToMain").text();
+	var val = $("#inputJos").val();
+	var con = val*valori[from]/valori[to];
+	var res = val.toString() + " " + from + " = " + con + " " + to;
+    $("#rezultat-conversie").text(res);
+
     $('html, body').animate({
         scrollTop: $("#inputJos").offset().top
     }, 2000);
